40535343142603901a9b649b2a29ac666221e326
muhammad farroos <muhammadfarroos12@gmail.com>
2021-11-17 14:40:02 +0700

    solve text color

--- app/src/main/java/com/farroos/doadandzikir/adapter/ArtikelAdapter.kt
+++ app/src/main/java/com/farroos/doadandzikir/adapter/ArtikelAdapter.kt
@@ -15,7 +15,7 @@ class ArtikelAdapter(private val listArtikel: ArrayList<Artikel>) :
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imgArtikel: ImageView = view.findViewById(R.id.img_artikel)
     }
-
+    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.row_item_artikel, parent, false)
